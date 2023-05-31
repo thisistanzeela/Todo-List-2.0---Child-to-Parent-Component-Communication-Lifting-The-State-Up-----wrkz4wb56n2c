d932bf9e81b7f746b9b40dc5bb81caeae88dd66d
thisistanzeela <86124816+thisistanzeela@users.noreply.github.com>
2023-05-31 23:38:09 +0530

    Update Today.js

--- src/components/Today.js
+++ src/components/Today.js
@@ -1,12 +1,27 @@
+// import React from "react";
+// import ListRender from "./ListRender";
+
+// const Today = (props) => {
+//   const date = new Date();
+  
+//   return (
+//     <div id="today-list">
+//       <ListRender list={} />
+//     </div>
+//   );
+// };
+
+// export default Today;
+
 import React from "react";
 import ListRender from "./ListRender";
 
 const Today = (props) => {
   const date = new Date();
-  
+  console.log("today date "+date)
   return (
     <div id="today-list">
-      <ListRender list={} />
+      <ListRender list={props.list.filter(li => li.date.toLocaleDateString() == date)} />
     </div>
   );
 };
