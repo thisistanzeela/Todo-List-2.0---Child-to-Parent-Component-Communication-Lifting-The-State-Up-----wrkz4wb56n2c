52f906474323c2cf9d93f611b033ca9e7264931b
thisistanzeela <86124816+thisistanzeela@users.noreply.github.com>
2023-06-05 20:45:48 +0530

    Update Inbox.js

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -1,94 +1,46 @@
-// import React, { useState, useRef } from "react";
-
-// const Inbox = (props) => {
- 
-
-//   return (
-//     <div>
-//       <h3>Inbox</h3>
-//       {!newTask && (
-//         <button className="new" onClick={} id='add-new'>
-//           +Add New
-//         </button>
-//       )}
-//       {newTask && (
-//         <form className="newtask-box">
-//           <input type="text" id="title" ref={}></input>
-//           <div className="buttons">
-//             <button className="new" id="add-list" onClick={}>
-//               Add Task
-//             </button>
-//             <button className="new" onClick={}>
-//               Cancel
-//             </button>
-//             <input
-//               type="date"
-//               ref={}
-//               defaultValue="2022-09-27"
-//               id="date"
-//             ></input>
-//           </div>
-//         </form>
-//       )}
-//       <div id="inbox">
-//         {props.list.map((list) => {
-//           return (
-//             <div className="box" key={}>
-//               <div className="task">
-//                 {} ({})
-//               </div>
-//             </div>
-//           );
-//         })}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Inbox;
-
-
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const Inbox = (props) => {
-  const [newTask,setNewTask] = useState(false)
-  const createNewTaskRef = useRef();
-  const addDateRef = useRef()
-  const showAddTaskHandler = () => {
-    console.log("add new Task")
-    setNewTask(!newTask)
+ 
+  const [newTask, setTask] = useState(false);
+ 
+  const [ele, setele] = useState(null);
+  const [added, setadded] =useState(4);
+  const text = useRef(null);
+  const ddate = useRef(null);
+  function updateText(e){
+    e.preventDefault();
+    setele({number: added, title: text.current.value, date: (new Date(ddate.current.value)).toLocaleDateString("en-US")})
+}
 
-  }
-  const addTaskHandler = (event) => {
-    event.preventDefault();
-    const titlee = createNewTaskRef.current.value;
-    const datee = addDateRef.current.value;
-    console.log(datee, titlee)
-    props.append([...props.list, {title:titlee, date:new Date(datee)}])
-    createNewTaskRef.current.value=''
-  }
-
+useEffect(()=>{
+    if (ele) {
+        props.append(ele);
+    }
+},[ele])
+  
   return (
+    
     <div>
       <h3>Inbox</h3>
       {!newTask && (
-        <button className="new" onClick={showAddTaskHandler} id='add-new'>
+        <button className="new" onClick={()=>{setTask(true)}} id='add-new'>
           +Add New
         </button>
       )}
       {newTask && (
         <form className="newtask-box">
-          <input type="text" id="title" ref={createNewTaskRef} />
+          <input type="text" id="title" ref={text}></input>
           <div className="buttons">
-            <button className="new" id="add-list" onClick={addTaskHandler}>
+            <button className="new" id="add-list" onClick={(e)=>{updateText(e)}}>
               Add Task
             </button>
-            <button className="new" onClick={() => {}}>
+            <button className="new" onClick={()=>{setTask(false)}}>
               Cancel
             </button>
             <input
               type="date"
-              ref={addDateRef}
+              ref={ddate}
               defaultValue="2022-09-27"
               id="date"
             ></input>
@@ -96,11 +48,11 @@ const Inbox = (props) => {
         </form>
       )}
       <div id="inbox">
-        {props.list.map((list,index) => {
+        {props.list.map((list) => {
           return (
-            <div className="box" key={index}>
+            <div className="box" key={list.number}>
               <div className="task">
-                {list.title} {"("+list.date.toLocaleDateString()+")"}
+                {list.title} ({list.date})
               </div>
             </div>
           );
