c52d6e7aa67b5efed9d6fbf89c20b14b83515cba
thisistanzeela <86124816+thisistanzeela@users.noreply.github.com>
2023-06-05 20:45:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,23 @@
-// import React, { useState } from 'react'
-// import SideNav from './SideNav';
-// import MainSection from './MainSection';
-// import Header from './Header';
-// import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
-// import '../styles/App.css';
-// const App = () => {
-
-
-//   return (
-//     <div id="main">
-//     <Header />
-//       <div className='row'>
-//         <div className='col-md-2'>
-//           <SideNav change={}/>
-//         </div>
-//         <div className='col-md-10'>
-//           <MainSection active = {}/>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-
-// export default App;
-
-
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import SideNav from './SideNav';
 import MainSection from './MainSection';
 import Header from './Header';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../styles/App.css';
-
 const App = () => {
-  const [activeMenu,setActiveMenu] = useState("INBOX")
-  const changeActiveMenu = (changeTo) => {
-    setActiveMenu(changeTo)
+  const [state, setState] = useState("INBOX");
+  function ChangeItems(inp){
+     setState(inp);
   }
   return (
     <div id="main">
     <Header />
       <div className='row'>
         <div className='col-md-2'>
-          <SideNav change={changeActiveMenu}/>
+          <SideNav change={ChangeItems}/>
         </div>
         <div className='col-md-10'>
-          <MainSection active = {activeMenu}/>
+          <MainSection active = {state}/>
         </div>
       </div>
     </div>
